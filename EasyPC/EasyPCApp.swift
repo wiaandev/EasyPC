5641c98b2ed9a16c9843fa1f0c973beb2eef74a6
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2022-10-09 15:27:49 +0200

    Initial Commit

---
+++ EasyPC/EasyPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPCApp.swift
+//  EasyPC
+//
+//  Created by Wiaan Duvenhage on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct EasyPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
