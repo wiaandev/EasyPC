09dc3809a99d9cb4d1f8a05380b89b636331a83c
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2022-10-11 12:59:06 +0200

    fixed navigation + splash + settings

--- EasyPC/Screens/ContentView.swift
+++ EasyPC/Screens/ContentView.swift
@@ -9,45 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var searchText = ""
+    @State var navigationBarBackButton = true
     var buildFilter: buildsFilter = buildTypes[1]
     var body: some View {
-        VStack{
-            ScrollView(.vertical){
-                HStack{
-                    Spacer()
-                    NavigationLink(destination: SettingsView()){
-                        Button(action: {
-                            print("button pressed")
-                        }){
-                            Image(systemName: "gear")
-                                .foregroundColor(.white)
-                        }
-                        .frame(width: 25, height: 25, alignment: .center)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color("Purple"))
-                        .clipShape(Rectangle())
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-                    } // end of navigationLink
-                }
-                HStack{
-                    TextField("Search builds here", text: $searchText)
-                        .padding(35)
-                        .overlay(RoundedRectangle(cornerRadius:     15).stroke(Color("Purple"))
+        NavigationView{
+            VStack{
+                ScrollView(.vertical){
+                    HStack{
+                        Spacer()
+                        NavigationLink(destination: SettingsView().navigationBarBackButtonHidden(true)){
+                            Button(action: {
+                            }){
+                                Image(systemName: "gear")
+                                    .foregroundColor(.white)
+                            }
+                            .frame(width: 25, height: 25, alignment: .center)
                             .padding()
-                        )
-                }
-                HStack{
-                    Text("Quick Filter")
-                        .foregroundColor(Color("Purple"))
-                        .padding(.horizontal)
-                        .font(.custom(FontsManager.Outfit.regular, size: 16))
-                    Spacer()
-                }
-                ScrollView(.horizontal){
-                    HStack(spacing: 0){
-                        Button("\(buildFilter.name)", action: {
+                            .foregroundColor(.white)
+                            .background(Color("Purple"))
+                            .clipShape(Rectangle())
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        }.navigationBarBackButtonHidden(true) // end of navigationLink
+                    }
+                    HStack{
+                        TextField("Search builds here", text: $searchText)
+                            .padding(35)
+                            .overlay(RoundedRectangle(cornerRadius:     15).stroke(Color("Purple"))
+                                .padding()
+                            )
+                    }
+                    HStack{
+                        Text("Quick Filter")
+                            .foregroundColor(Color("Purple"))
+                            .padding(.horizontal)
+                            .font(.custom(FontsManager.Outfit.regular, size: 16))
+                        Spacer()
+                    }
+                    ScrollView(.horizontal){
+                        HStack(spacing: 0){
+                            Button("\(buildFilter.name)", action: {
                                 print("Hello World")
                             })
                             .frame(width: 75, height: 25)
@@ -56,140 +57,142 @@ struct ContentView: View {
                             .overlay(
                                 RoundedRectangle(cornerRadius: 10).stroke(Color("Purple"), lineWidth: 1))
                             .padding(.horizontal)
-
-                        Button{
                             
-                        } label: {
-                            VStack{
-                                Image(systemName: "\(buildFilter.icon)")
-                                Text("Gaming")
+                            Button{
+                                
+                            } label: {
+                                VStack{
+                                    Image(systemName: "\(buildFilter.icon)")
+                                    Text("Gaming")
+                                }
+                                
                             }
-
+                            .frame(width: 75, height: 25)
+                            .padding()
+                            .foregroundColor(Color("Purple"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10).stroke(Color("Purple"), lineWidth: 1))
+                            .padding(.horizontal)
+                            Button("Gaming", action: {
+                                print("Hello World")
+                            })
+                            .frame(width: 75, height: 25)
+                            .padding()
+                            .foregroundColor(Color("Purple"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10).stroke(Color("Purple"), lineWidth: 1))
+                            .padding(.horizontal)
                         }
-                        .frame(width: 75, height: 25)
-                        .padding()
-                        .foregroundColor(Color("Purple"))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10).stroke(Color("Purple"), lineWidth: 1))
-                        .padding(.horizontal)
-                        Button("Gaming", action: {
-                            print("Hello World")
-                        })
-                        .frame(width: 75, height: 25)
-                        .padding()
-                        .foregroundColor(Color("Purple"))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10).stroke(Color("Purple"), lineWidth: 1))
-                        .padding(.horizontal)
+                        
                     }
-
-                }
-                
-                HStack{
-                    Text("Our Builds")
+                    
+                    HStack{
+                        Text("Our Builds")
+                            .padding(.horizontal)
+                            .foregroundColor(Color("Purple"))
+                            .font(.system(size: 40))
+                            .fontWeight(.bold)
+                            .foregroundStyle(                    LinearGradient(
+                                colors: [Color("Purple"), Color("Blue")],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                            )
+                        Spacer()
+                    }
+                    HStack{
+                        Text("Choose a build type")
+                            .padding(.horizontal)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color("Blue"))
+                        Spacer()
+                    }
+                    Divider()
                         .padding(.horizontal)
+                    Image("gaming")
+                        .resizable()
+                        .padding(.horizontal)
+                        .cornerRadius(23)
+                        .aspectRatio(contentMode: .fit)
                         .foregroundColor(Color("Purple"))
-                        .font(.system(size: 40))
-                        .fontWeight(.bold)
-                        .foregroundStyle(                    LinearGradient(
-                            colors: [Color("Purple"), Color("Blue")],
-                            startPoint: .top,
-                            endPoint: .bottom
+                        .overlay(
+                            ZStack{
+                                Color("Purple")
+                                    .opacity(0.7)
+                                    .cornerRadius(20)
+                                    .padding(.horizontal)
+                                VStack{
+                                    VStack(alignment: .leading, spacing: 0){
+                                        Text("Gaming")
+                                            .foregroundColor(Color("White"))
+                                            .font(.system(size: 36))
+                                            .fontWeight(.bold)
+                                            .padding(.horizontal)
+                                        Text("Choose this build type to see everything you would need for a gaming build")
+                                            .padding()
+                                            .foregroundColor(Color("White"))
+                                    }// end of HStack
+                                    HStack{
+                                        Spacer()
+                                        Button("Select Build"){
+                                            
+                                        }
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Color("Blue"))
+                                        .clipShape(Capsule())
+                                        .padding(.horizontal, 25)
+                                    }
+                                    
+                                } // end of VStack
+                                
+                            }
                         )
-                        )
-                    Spacer()
-                }
-                HStack{
-                    Text("Choose a build type")
+                    
+                    Image("gaming")
+                        .resizable()
                         .padding(.horizontal)
-                        .font(.system(size: 20))
-                        .foregroundColor(Color("Blue"))
-                    Spacer()
+                        .cornerRadius(23)
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color("Purple"))
+                        .overlay(
+                            ZStack{
+                                Color("Purple")
+                                    .opacity(0.7)
+                                    .cornerRadius(20)
+                                    .padding(.horizontal)
+                                VStack{
+                                    VStack(alignment: .leading, spacing: 0){
+                                        Text("Gaming")
+                                            .foregroundColor(Color("White"))
+                                            .font(.system(size: 36))
+                                            .fontWeight(.bold)
+                                            .padding(.horizontal)
+                                        Text("Choose this build type to see everything you would need for a gaming build")
+                                            .padding()
+                                            .foregroundColor(Color("White"))
+                                    }// end of HStack
+                                    HStack{
+                                        Spacer()
+                                        Button("Select Build"){
+                                            
+                                        }
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Color("Blue"))
+                                        .clipShape(Capsule())
+                                        .padding(.horizontal, 25)
+                                    }
+                                    
+                                } // end of VStack
+                                
+                            }
+                        )
                 }
-                Divider()
-                    .padding(.horizontal)
-                Image("gaming")
-                    .resizable()
-                    .padding(.horizontal)
-                    .cornerRadius(23)
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color("Purple"))
-                    .overlay(
-                        ZStack{
-                            Color("Purple")
-                                .opacity(0.7)
-                                .cornerRadius(20)
-                                .padding(.horizontal)
-                            VStack{
-                                VStack(alignment: .leading, spacing: 0){
-                                    Text("Gaming")
-                                        .foregroundColor(Color("White"))
-                                        .font(.system(size: 36))
-                                        .fontWeight(.bold)
-                                        .padding(.horizontal)
-                                    Text("Choose this build type to see everything you would need for a gaming build")
-                                        .padding()
-                                        .foregroundColor(Color("White"))
-                                }// end of HStack
-                                HStack{
-                                    Spacer()
-                                    Button("Select Build"){
-                                        
-                                    }
-                                    .padding()
-                                    .foregroundColor(.white)
-                                    .background(Color("Blue"))
-                                    .clipShape(Capsule())
-                                    .padding(.horizontal, 25)
-                                }
-
-                            } // end of VStack
-
-                        }
-                    )
-                
-                Image("gaming")
-                    .resizable()
-                    .padding(.horizontal)
-                    .cornerRadius(23)
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(Color("Purple"))
-                    .overlay(
-                        ZStack{
-                            Color("Purple")
-                                .opacity(0.7)
-                                .cornerRadius(20)
-                                .padding(.horizontal)
-                            VStack{
-                                VStack(alignment: .leading, spacing: 0){
-                                    Text("Gaming")
-                                        .foregroundColor(Color("White"))
-                                        .font(.system(size: 36))
-                                        .fontWeight(.bold)
-                                        .padding(.horizontal)
-                                    Text("Choose this build type to see everything you would need for a gaming build")
-                                        .padding()
-                                        .foregroundColor(Color("White"))
-                                }// end of HStack
-                                HStack{
-                                    Spacer()
-                                    Button("Select Build"){
-                                        
-                                    }
-                                    .padding()
-                                    .foregroundColor(.white)
-                                    .background(Color("Blue"))
-                                    .clipShape(Capsule())
-                                    .padding(.horizontal, 25)
-                                }
-
-                            } // end of VStack
-
-                        }
-                    )
             }
         }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
--- EasyPC/Screens/SettingsView.swift
+++ EasyPC/Screens/SettingsView.swift
@@ -8,63 +8,79 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var showGreeting = false
     var body: some View {
-        ScrollView(.vertical){
-            VStack{
-                HStack(spacing: 0){
-                    Image(systemName: "chevron.left")
-                        .padding()
-                        .foregroundColor(Color("Purple"))
-                    Text("Go Back")
-                        .foregroundColor(Color("Purple"))
-                    Spacer()
-                }
-                HStack(spacing: 0){
-                    Text("Settings")
-                        .fontWeight(.bold)
-                        .font(.system(size: 45))
-                        .padding([.horizontal, .bottom])
-                    Spacer()
-                }
-                HStack(spacing: 0){
-                    Text("About The App")
-                        .padding(.horizontal)
-                    Spacer()
-                }
-                HStack(spacing: 0){
-                    Text("Hi! Welcome to EasyPC. This is an app that guides users on building their very own computer. Whether it be for personal use or something that you want to use for work, EasyPC can help and show you that it is not as scary and daunting as it looks.")
-                        .padding()
-                }
-                Divider()
-                    .padding(.horizontal)
-                HStack{
-                    Text("Appearance")
-                        .padding()
-                    Spacer()
-                }
-                HStack{
-                    Text("Light")
-                        .padding()
-                    Text("Dark")
-                        .padding()
-                    Spacer()
-                }
-                Divider()
-                HStack{
-                    Text("The Developer")
-                        .padding()
-                    Spacer()
-                }
+        NavigationView{
+            ScrollView(.vertical){
                 VStack{
-                    Text("Hi, my name is Wiaan Duvenhage! I am a 2nd year full-stack developer student at The Open Window")
-                        .padding(.horizontal)
-                    Spacer()
-                    Text("What do you think about BuildBuddy? Let me know by reporting a bug on my github  page linked below")
-                        .padding(.horizontal)
-                }
+                    HStack(spacing: 0){
+                    NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true)){
 
+                            Image(systemName: "chevron.left")
+                                .padding()
+                                .foregroundColor(Color("Purple"))
+                            Text("Go Back")
+                                .foregroundColor(Color("Purple"))
+
+                        }
+                        Spacer()
+                    }
+                    HStack(spacing: 0){
+                        Text("Settings")
+                            .fontWeight(.bold)
+                            .font(.system(size: 45))
+                            .padding([.horizontal, .bottom])
+                        Spacer()
+                    }
+                    HStack(spacing: 0){
+                        Text("About The App")
+                            .padding(.horizontal)
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    HStack(spacing: 0){
+                        Text("Hi! Welcome to EasyPC. This is an app that guides users on building their very own computer. Whether it be for personal use or something that you want to use for work, EasyPC can help and show you that it is not as scary and daunting as it looks.")
+                            .padding()
+                    }
+                    Divider()
+                        .padding(.horizontal)
+                    HStack{
+                        Text("Appearance")
+                            .fontWeight(.bold)
+                            .padding()
+                        Spacer()
+                    }
+                    HStack{
+                        Toggle("Dark Mode" ,isOn: $showGreeting)
+                            .toggleStyle(SwitchToggleStyle(tint: Color("Purple")))
+                            .padding(.horizontal)
+                        Spacer()
+                    }
+                    Divider()
+                    HStack{
+                        Text("The Developer")
+                            .fontWeight(.bold)
+                            .padding()
+                        Spacer()
+                    }
+                    VStack(spacing: 10){
+                        Text("Hi, my name is Wiaan Duvenhage! I am a 2nd year full-stack developer student at The Open Window")
+                            .padding(.horizontal)
+                        Text("What do you think about EasyPC? Let me know by reporting a bug on my github  page linked below")
+                            .padding(.horizontal)
+                        
+                        Image("wiaan")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70)
+                            .cornerRadius(15)
+                        Text("This is me")
+                    }
+
+                }
             }
         }
+
     }
 }
 
